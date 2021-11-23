df6929c341b3e0b0a9bc6c4059d68f23557d36df
Elod044 <33983644+Elod044@users.noreply.github.com>
2021-11-23 18:11:37 +0100

    Create config.py

---
+++ config.py
@@ -0,0 +1,4 @@
+USERNAME = "Your router's username, usually admin"
+PASSWORD = "Your router's password, usually the password that you use to log in to the web interface"
+PORT = "The SSH port of your router"
+HOST = "The ip address of your router, usually 192.168.1.1"
